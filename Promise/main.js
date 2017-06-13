ecd787bec3d100964db071d97aa54a5607582fba
SergeyDenisiuk <seruy722@gmail.com>
2017-06-14 01:33:16 +0300

    Promise part2

--- Promise/main.js
+++ Promise/main.js
@@ -1,13 +1,32 @@
 function applyVisa(documents) {
-    console.log('Обработка данных');
+    console.log("Обработка данных...");
     let promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            Math.random() > 0.5 ?
-                resolve("Виза получена!") :
-                reject("В визе отказано!");
+            Math.random() > 0.5 ? resolve({}) : reject("Отказ");
         }, 2000);
     });
     return promise;
 }
 
-applyVisa({}).then(visa => console.log(visa), reson => console.error(reson));
+function bookHotel(visa) {
+    console.log('Отель забронирован');
+    console.log(visa);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(visa), 2000);
+    });
+}
+
+function buyTikets(visa) {
+    console.log('Билеты куплены');
+    console.log(visa);
+}
+
+applyVisa({})
+    .then(visa => {
+        console.log("Виза получена!");
+        console.log(visa);
+        return visa;
+    })
+    .then(bookHotel)
+    .then(buyTikets)
+    .catch(err => console.error(err));
